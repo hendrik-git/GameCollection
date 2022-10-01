88b393754fd56ebbf4292edf31434ccb0577ebbd
Hendrik Pöttker <hendrik.poettker@web.de>
2022-10-01 15:01:49 +0200

    Added CMakeGraphVizOptions.cmake

---
+++ CMakeGraphVizOptions.cmake
@@ -0,0 +1,45 @@
+# The graph name. Default: value of CMAKE_PROJECT_NAME
+#set(GRAPHVIZ_GRAPH_NAME ${CMAKE_PROJECT_NAME})
+
+# The header written at the top of the Graphviz files.
+# Default: "node [ fontsize = "12" ];"
+set(GRAPHVIZ_GRAPH_HEADER "node [ fontname=\"DejaVu Sans Mono\"; fontsize = \"18\" ];")
+
+# The prefix for each node in the Graphviz files. Default: "node"
+# set(GRAPHVIZ_NODE_PREFIX node)
+
+# Set to FALSE to exclude executables from the generated graphs.
+# set(GRAPHVIZ_EXECUTABLES TRUE)
+
+# Set to FALSE to exclude static libraries from the generated graphs.
+# set(GRAPHVIZ_STATIC_LIBS TRUE)
+
+# Set to FALSE to exclude shared libraries from the generated graphs.
+# set(GRAPHVIZ_SHARED_LIBS TRUE)
+
+# Set to FALSE to exclude module libraries from the generated graphs.
+# set(GRAPHVIZ_MODULE_LIBS TRUE)
+
+# Set to FALSE to exclude interface libraries from the generated graphs.
+# set(GRAPHVIZ_INTERFACE_LIBS TRUE)
+
+# Set to FALSE to exclude object libraries from the generated graphs.
+# set(GRAPHVIZ_OBJECT_LIBS TRUE)
+
+# Set to FALSE to exclude unknown libraries from the generated graphs.
+# set(GRAPHVIZ_UNKNOWN_LIBS TRUE)
+
+# Set to FALSE to exclude external libraries from the generated graphs.
+# set(GRAPHVIZ_EXTERNAL_LIBS TRUE)
+
+# Set to TRUE to include custom targets in the generated graphs.
+set(GRAPHVIZ_CUSTOM_TARGETS TRUE)
+
+# A list of regular expressions for names of targets to exclude from the generated graphs.
+#set(GRAPHVIZ_IGNORE_TARGETS "")
+
+# Set to FALSE to not generate per-target graphs foo.dot.<target>.
+set(GRAPHVIZ_GENERATE_PER_TARGET FALSE)
+
+# Set to FALSE to not generate depender graphs foo.dot.<target>.dependers.
+set(GRAPHVIZ_GENERATE_DEPENDERS FALSe)
