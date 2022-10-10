ce6a95d4b95728a89729174a11debc56fc4f002e
hendrik-git <58333293+hendrik-git@users.noreply.github.com>
2022-10-10 19:34:33 +0200

    Added new tool "cmake-format"

---
+++ cmake/cmake-format.cmake
@@ -0,0 +1,21 @@
+# -------------------------------------------------------------------------------------------------
+# cmake formatter
+# -------------------------------------------------------------------------------------------------
+find_program(EXE_CMAKEFORMAT "cmake-format")
+if(NOT EXE_CMAKEFORMAT)
+    message(STATUS "cmake-format executable not found! ${EXE_CMAKEFORMAT}")
+    message(STATUS "-- Download with pip install cmakelang")
+else()
+    message(STATUS "cmake-format executable found! Adding target.")
+
+    # find all cmake files to be formated
+    file(GLOB_RECURSE CMAKE_FILE_LIST CMakeLists.txt *.cmake)
+
+    add_custom_target(
+        cmake-format ALL
+        COMMAND cmake-format -c ${CMAKE_SOURCE_DIR}/tools/cmake-format.py -i
+                ${CMAKE_FILE_LIST}
+        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+    )
+    set_target_properties(cmake-format PROPERTIES FOLDER Tools)
+endif()
