f96d7b8b9ae188892b3a3819f31ce37ca70f3030
hendrik-git <58333293+hendrik-git@users.noreply.github.com>
2022-10-11 23:40:12 +0200

    Added basic command line parsing to main()

--- app/main.cpp
+++ app/main.cpp
@@ -1,23 +1,54 @@
+#include "CLI/App.hpp"
+#include "CLI/Config.hpp"
+#include "CLI/Formatter.hpp"
 #include <SFML/Graphics.hpp>
 
-int main()
+using namespace std::string_literals;
+
+int main(int argc, char** argv)
 {
-	sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-	sf::CircleShape	 shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	//!---------------------------------------------------------------------------------------------
+	//! Parse command line arguments
+	//!---------------------------------------------------------------------------------------------
+	// see https://cliutils.github.io/CLI11/book/
+	CLI::App app{"Game collection"};
 
-	while(window.isOpen())
+	auto inputFile{"default"s};
+
+	app.add_option("-f,--file", inputFile, "File to load");
+
+	auto quit = app.add_flag("-q,--quit", "Immediately exit program");
+
+	CLI11_PARSE(app, argc, argv);
+
+	// --------------------------------------------------------------------------------------------
+	// Start the application
+	// --------------------------------------------------------------------------------------------
+	if(!quit->as<bool>())
 	{
-		sf::Event event;
-		while(window.pollEvent(event))
-		{
-			if(event.type == sf::Event::Closed)
-				window.close();
-		}
+		std::cout << "Starting application" << std::endl;
 
-		window.clear();
-		window.draw(shape);
-		window.display();
+		sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+		sf::CircleShape	 shape(100.f);
+		shape.setFillColor(sf::Color::Green);
+
+		while(window.isOpen())
+		{
+			sf::Event event;
+			while(window.pollEvent(event))
+			{
+				if(event.type == sf::Event::Closed)
+					window.close();
+			}
+
+			window.clear();
+			window.draw(shape);
+			window.display();
+		}
+	}
+	else
+	{
+		std::cout << "Quitting application" << std::endl;
 	}
 
 	return 0;
