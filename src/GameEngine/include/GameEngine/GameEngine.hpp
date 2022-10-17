e0d0597bba8854f86289d73b2611421523a2ed16
hendrik-git <58333293+hendrik-git@users.noreply.github.com>
2022-10-17 22:59:08 +0200

    Added functions to the  GameEngine class

--- src/GameEngine/include/GameEngine/GameEngine.hpp
+++ src/GameEngine/include/GameEngine/GameEngine.hpp
@@ -2,21 +2,33 @@
 
 #pragma once
 #include "EntityManager.hpp"
+#include <SFML/Graphics.hpp>
 
 class GameEngine
 {
   public:
+	void init() {}
+
 	void main_loop()
 	{
-		manager_.update();
-		// user input
-		// movement
-		// collision
-		// render
 		current_frame_++;
+		manager_.update();
+		user_input();
+		movement();
+		collision();
+		render();
 	}
 
+	void user_input() {}
+	void movement() {}
+	void collision() {}
+	void render() {}
+
   private:
-	EntityManager manager_;
-	size_t		  current_frame_ = 0;
+	sf::RenderWindow window_;
+	EntityManager	 manager_;
+	Entity			 player_;
+	bool			 paused_;
+	bool			 running_;
+	size_t			 current_frame_ = 0;
 };
