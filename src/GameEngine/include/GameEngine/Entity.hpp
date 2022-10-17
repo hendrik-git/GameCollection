36ed64505c8ab085ef7937a7f8dafdc8eb11d263
hendrik-git <58333293+hendrik-git@users.noreply.github.com>
2022-10-17 22:58:11 +0200

    Added functions to the Entity class

--- src/GameEngine/include/GameEngine/Entity.hpp
+++ src/GameEngine/include/GameEngine/Entity.hpp
@@ -1,12 +1,35 @@
 /// @file Entity
 
 #pragma once
+#include "Component.hpp"
 #include <string_view>
+#include <memory>
 
 class Entity
 {
   public:
 	// shared pointers to the components will come here
+	std::shared_ptr<Transform> transform_;
+
+	void destroy()
+	{
+		alive_ = false;
+	}
+
+	[[nodiscard]] auto id() const -> size_t
+	{
+		return id_;
+	}
+
+	[[nodiscard]] auto tag() const -> std::string_view
+	{
+		return tag_;
+	}
+
+	[[nodiscard]] auto is_alive() const -> bool
+	{
+		return alive_;
+	}
 
   private:
 	// private constructor allows only the EntityManager to create instances
