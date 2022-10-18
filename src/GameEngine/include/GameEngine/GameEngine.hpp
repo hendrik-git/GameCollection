84e460a344d8bb6232d93aa25024442b4d725564
hendrik-git <58333293+hendrik-git@users.noreply.github.com>
2022-10-18 22:39:59 +0200

    Added implementations to the GameEngine

--- src/GameEngine/include/GameEngine/GameEngine.hpp
+++ src/GameEngine/include/GameEngine/GameEngine.hpp
@@ -7,28 +7,54 @@
 class GameEngine
 {
   public:
+	GameEngine()
+	{
+		window_.create(sf::VideoMode({400, 400}), "GameCollection");
+	}
+
 	void init() {}
 
 	void main_loop()
 	{
-		current_frame_++;
-		manager_.update();
-		user_input();
-		movement();
-		collision();
-		render();
+		while(window_.isOpen())
+		{
+			current_frame_++;
+			manager_.update();
+			user_input();
+			movement();
+			collision();
+			render();
+		}
 	}
 
-	void user_input() {}
+	void user_input()
+	{
+		sf::Event event;
+		while(window_.pollEvent(event))
+		{
+			if(event.type == sf::Event::Closed)
+			{
+				window_.close();
+			}
+		}
+	}
 	void movement() {}
 	void collision() {}
-	void render() {}
+	void render()
+	{
+		sf::CircleShape shape(100.f);
+		shape.setFillColor(sf::Color::Green);
+
+		window_.clear();
+		window_.draw(shape);
+		window_.display();
+	}
 
   private:
 	sf::RenderWindow window_;
 	EntityManager	 manager_;
-	Entity			 player_;
-	bool			 paused_;
-	bool			 running_;
+	bool			 paused_		= false;
+	bool			 running_		= false;
 	size_t			 current_frame_ = 0;
+	std::shared_ptr<Entity> player_; ///< for convenient access to the player entity
 };
