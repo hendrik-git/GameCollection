81c6f00c383c831fe9dfd19047e94dd47528d8b5
hendrik-git <58333293+hendrik-git@users.noreply.github.com>
2022-10-22 21:09:30 +0200

    Improved main function

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,7 @@
-#include "CLI/App.hpp"
-#include "CLI/Config.hpp"
-#include "CLI/Formatter.hpp"
+#include <CLI/App.hpp>
+#include <CLI/Config.hpp>
+#include <CLI/Formatter.hpp>
 #include <GameEngine/GameEngine.hpp>
-#include <SFML/Graphics.hpp>
 
 using namespace std::string_literals;
 
@@ -14,14 +13,17 @@ int main(int argc, char** argv)
 	// see https://cliutils.github.io/CLI11/book/
 	CLI::App app{"Game collection"};
 
+	auto gameSelect{""s};
 	auto inputFile{"default"s};
 
-	app.add_option("-f,--file", inputFile, "File to load");
+	app.add_option("-g,--game", gameSelect, "Which game to load");
+	app.add_option("-f,--file", inputFile, "Settings file to load");
 
 	[[maybe_unused]] auto list = app.add_flag("-l,--list", "List all available games");
 	[[maybe_unused]] auto quit = app.add_flag("-q,--quit", "Immediately exit program");
 
 	CLI11_PARSE(app, argc, argv);
+
 
 	// --------------------------------------------------------------------------------------------
 	// Start the application
@@ -29,7 +31,7 @@ int main(int argc, char** argv)
 	if(!quit->as<bool>())
 	{
 		std::cout << "Test" << std::endl;
-		GameEngine engine{std::filesystem::path{}};
+		GameEngine engine{std::filesystem::path{inputFile}};
 		engine.run();
 	}
 	else
