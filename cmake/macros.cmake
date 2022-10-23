bd5b144fcd94f6d661d9c83d64c824ee375689d1
hendrik-git <58333293+hendrik-git@users.noreply.github.com>
2022-10-23 14:48:27 +0200

    Linted and formatted CMake files

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,7 +1,7 @@
 # This macro wraps a security check around the properties folder command, so that removing a target
 # does cause cmake to error trying to set a property (e.g. unchecking a part of the SFML library)
-macro(set_VS_folder targetName folderName)
+macro(SetFolderInVS targetName folderName)
     if(TARGET ${targetName})
         set_target_properties(${targetName} PROPERTIES FOLDER ${folderName})
     endif()
-endmacro(set_VS_folder)
+endmacro(SetFolderInVS)
