d5d2abdcf3ffe502349ccfa2d87eff6cbe693e35
Hendrik Pöttker <hendrik.poettker@web.de>
2022-11-01 13:25:05 +0100

    Added initial Scene class

---
+++ src/GameEngine/Scene.cpp
@@ -0,0 +1,39 @@
+#include <GameEngine/Scene.hpp>
+
+Scene::Scene(GameEngine* engine) {}
+
+// void Scene::simulate() {}
+
+void Scene::register_action() {}
+
+// size_t Scene::width()
+//{
+//	return size_t();
+//}
+//
+// size_t Scene::height()
+//{
+//	return size_t();
+//}
+
+bool Scene::has_ended()
+{
+	return has_ended_;
+}
+
+size_t Scene::current_frame()
+{
+	return current_frame_;
+}
+
+ActionMap& Scene::get_ActionMap()
+{
+	return action_map_;
+}
+
+// void Scene::draw_line(Vec2 p1, Vec2 p2) {}
+
+void Scene::set_paused(bool pause)
+{
+	is_paused_ = pause;
+}
---
+++ src/GameEngine/include/GameEngine/Scene.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include "Action.hpp"
+#include "EntityManager.hpp"
+#include <map>
+
+
+using ActionMap = std::map<int, std::string>;
+
+class GameEngine;  // forward declare GameEngine class for pointer
+
+class Scene
+{
+  public:
+	Scene() = default;
+	Scene(GameEngine* engine);
+
+	virtual void update()				   = 0;
+	virtual void render()				   = 0;
+	virtual void do_action(Action& action) = 0;
+
+	// void simulate();
+	void register_action();
+
+	// size_t	   width();
+	// size_t	   height();
+
+	bool	   has_ended();
+	size_t	   current_frame();
+	ActionMap& get_ActionMap();
+
+	// void	   draw_line(Vec2 p1, Vec2 p2);
+
+  protected:
+	virtual void on_end() = 0;
+	void		 set_paused(bool pause);
+
+	ActionMap	  action_map_;
+	EntityManager entities_;
+	GameEngine*	  game_			 = nullptr;
+	bool		  is_paused_	 = false;
+	bool		  has_ended_	 = false;
+	size_t		  current_frame_ = 0;
+};
