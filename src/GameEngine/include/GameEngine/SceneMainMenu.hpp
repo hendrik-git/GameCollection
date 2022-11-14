e79f7ce0f3c11d7ce3cc7d5d0b0b7c6f8fb7435c
Hendrik Pöttker <hendrik.poettker@web.de>
2022-11-14 16:29:24 +0100

    Added new files for main menu scene

---
+++ src/GameEngine/include/GameEngine/SceneMainMenu.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "Scene.hpp"
+
+class SceneMainMenu : public Scene
+{
+  public:
+	SceneMainMenu(GameEngine* engine) : Scene(engine)
+	{
+		init();
+	};
+
+	void init();
+	void update() override;
+	void render() override;
+	void do_action(const Action& action) override;
+	void on_end() override;
+
+	sf::Text   text_;
+	sf::Sprite background_;
+	int		   selection_;
+};
