b0342dd4aae5e728a0cd9ceb0c6550ee2a31069d
hendrik-git <58333293+hendrik-git@users.noreply.github.com>
2022-12-03 18:35:45 +0100

    Fixed graphviz target being dependent on working directory

--- cmake/graphviz.cmake
+++ cmake/graphviz.cmake
@@ -11,7 +11,7 @@ else()
     add_custom_target(
         graphviz ALL
         COMMAND ${CMAKE_COMMAND} "--graphviz=GameCollectionApp.dot" .
-        COMMAND dot -Tpng GameCollectionApp.dot -o ../docs/GameCollectionApp.png
+        COMMAND dot -Tpng GameCollectionApp.dot -o ${CMAKE_SOURCE_DIR}/docs/GameCollectionApp.png
         WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
         COMMENT "Generating dependency graph in docs/"
     )
