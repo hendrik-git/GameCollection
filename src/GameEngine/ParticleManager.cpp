5c7554bf95262f604ce8ac98334da1fae90cc652
Hendrik Pöttker <hendrik.poettker@web.de>
2022-12-18 10:52:54 +0100

    Remade the ParticleManager and Particle classes

--- src/GameEngine/ParticleManager.cpp
+++ src/GameEngine/ParticleManager.cpp
@@ -1,13 +1,1 @@
 #include <GameEngine/ParticleManager.hpp>
-
-void ParticleManager::draw(sf::RenderTarget& target, const sf::RenderStates& states) const 
-{
-	// apply the transform
-	//states.transform *= getTransform();
-
-	// our particles don't use a texture
-	//states.texture = NULL;
-
-	// draw the vertex array
-	target.draw(vertices_, states);
-}
--- src/GameEngine/include/GameEngine/ParticleManager.hpp
+++ src/GameEngine/include/GameEngine/ParticleManager.hpp
@@ -8,86 +8,103 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-class ParticleManager
-	: public sf::Drawable
-	, public sf::Transformable
+
+class Particle : public sf::Drawable
 {
   public:
-	ParticleManager(unsigned int count = 100)
-		: particles_(count)
-		, vertices_(sf::Points, count)
-		, lifetime_(sf::seconds(3.f))
-		, emitter_(0.f, 0.f)
+	Particle(){};
+	~Particle(){};
+
+	void init(float x, float y, float xVel, float yVel, int lifetime)
 	{
+		x_			= x;
+		y_			= y;
+		dx_			= xVel;
+		dy_			= yVel;
+		framesLeft_ = lifetime;
 	}
 
-	void set_emitter(sf::Vector2f position)
+	void update()
 	{
-		emitter_ = position;
-	}
-
-	void update(sf::Time elapsed)
-	{
-
-		for(std::size_t i = 0; i < particles_.size(); ++i)
+		if(!in_use())
 		{
-			// update the particle lifetime
-			Particle& p = particles_[i];
-			p.lifetime -= elapsed;
+			return;
+		}
 
-			// if the particle is dead, respawn it
-			if(p.lifetime <= sf::Time::Zero)
-				reset_particle(i);
+		framesLeft_--;
+		x_ += dx_;
+		y_ += dy_;
+	}
 
-			// update the position of the corresponding vertex
-			vertices_[i].position += p.velocity * elapsed.asSeconds();
+	bool in_use() const
+	{
+		return framesLeft_ > 0;
+	}
 
-			// update the alpha (transparency) of the particle according to its lifetime
-			float ratio			 = p.lifetime.asSeconds() / lifetime_.asSeconds();
-			vertices_[i].color.a = static_cast<uint8_t>(ratio * 255);
+	virtual void draw(sf::RenderTarget&		  target,
+					  const sf::RenderStates& states = sf::RenderStates::Default) const override
+	{
+		if(in_use())
+		{
+			sf::Vertex v;
+			v.position = sf::Vector2f(x_, y_);
+			v.color	   = sf::Color::Red;
+			target.draw(&v, 1, sf::Points, states);
 		}
 	}
 
-  protected:
-	// Inherited via Drawable
-	virtual void draw(sf::RenderTarget& target, const sf::RenderStates& states) const override;
-
-	// virtual void draw(RenderTarget& target, const RenderStates& states) const = 0;
-	// virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
-	//{
-	//	// apply the transform
-	//	states.transform *= getTransform();
-
-	//	// our particles don't use a texture
-	//	states.texture = NULL;
-
-	//	// draw the vertex array
-	//	target.draw(vertices_, states);
-	//}
-
   private:
-	/// @brief POD struct holding lifetime and velocity of a single particle
-	struct Particle
+	int	  framesLeft_ = 0;
+	float x_		  = 0.0;
+	float y_		  = 0.0;
+	float dx_		  = 0.0;
+	float dy_		  = 0.0;
+};
+
+
+/// @brief Is responsible for creating, storing, updating and drawing each particle
+class ParticleManager
+{
+  public:
+	ParticleManager(int size = 1'000)
 	{
-		sf::Vector2f velocity;
-		sf::Time	 lifetime;
+		particles_.resize(size);
 	};
 
-
-	void reset_particle(std::size_t index)
+	void create(float x, float y, float dx, float dy, int lifetime)
 	{
-		// give a random velocity and lifetime to the particle
-		float angle				   = (std::rand() % 360) * 3.14f / 180.f;
-		float speed				   = (std::rand() % 50) + 50.f;
-		particles_[index].velocity = sf::Vector2f(std::cos(angle) * speed, std::sin(angle) * speed);
-		particles_[index].lifetime = sf::milliseconds((std::rand() % 2000) + 1000);
-
-		// reset the position of the corresponding vertex
-		vertices_[index].position = emitter_;
+		// Find an available particle.
+		for(auto& particle : particles_)
+		{
+			if(!particle.in_use())
+			{
+				particle.init(x, y, dx, dy, lifetime);
+				return;
+			}
+		}
 	}
 
-	std::vector<Particle> particles_;
-	sf::VertexArray		  vertices_;
-	sf::Time			  lifetime_;
-	sf::Vector2f		  emitter_;
+	void update()
+	{
+		for(auto& particle : particles_)
+		{
+			particle.update();
+		}
+	}
+
+	void draw(sf::RenderTarget& target, const sf::RenderStates& states = sf::RenderStates::Default)
+	{
+		for(auto& particle : particles_)
+		{
+			particle.draw(target, states);
+		}
+	}
+
+	void resize(size_t max_particle_amount)
+	{
+		particles_.resize(max_particle_amount);
+	}
+
+  private:
+	std::vector<Particle>		 particles_;
 };
