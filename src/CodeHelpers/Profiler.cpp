39e98e4d4d989147b40bfb26be9803bb0fba691c
hendrik-git <58333293+hendrik-git@users.noreply.github.com>
2023-01-21 12:06:55 +0100

    Improved Profiler with suggestions from clang tidy and cppcheck

--- src/CodeHelpers/Profiler.cpp
+++ src/CodeHelpers/Profiler.cpp
@@ -1,15 +1,17 @@
 #include <CodeHelpers/Profiler.hpp>
+#include <format>
+#include <iostream>
 
-/*static*/ CodeHelper::Profiler& CodeHelper::Profiler::get()
+/*static*/ auto CodeHelper::Profiler::get() -> CodeHelper::Profiler&
 {
 	static Profiler instance;
 	return instance;
 }
 
-void CodeHelper::Profiler::write(const ProfileResult& result)
+void CodeHelper::Profiler::write(const profile_result& result)
 {
 	// ensure profiling from multiple thread works
-	std::lock_guard<std::mutex> lock(lock_);
+	std::scoped_lock lock(lock_);
 
 	// need to seperate each profile struct with a commata
 	if(profile_count_++ > 0)
@@ -18,7 +20,7 @@ void CodeHelper::Profiler::write(const ProfileResult& result)
 	}
 
 	auto name = result.name;
-	std::replace(name.begin(), name.end(), '"', '\'');
+	std::ranges::replace(name, '"', '\'');
 
 	// Write result JSON here
 	outfile_ << "\n  {";
@@ -32,16 +34,23 @@ void CodeHelper::Profiler::write(const ProfileResult& result)
 	outfile_ << "\n  }";
 }
 
-CodeHelper::Profiler::~Profiler()
+CodeHelper::Profiler::~Profiler() noexcept
 {
-	// Write JSON footer here
-	outfile_ << "\n]}";
+	try
+	{
+		// Write JSON footer here
+		outfile_ << "\n]}";
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 }
 
 CodeHelper::Profiler::Profiler()
 {
 	// Write JSON header here
-	outfile_ << "{\"otherData\": {}, \"traceEvents\":[";
+	outfile_ << R"({"otherData": {}, "traceEvents":[)";
 }
 
 CodeHelper::Timer::Timer(const std::string& name)
@@ -62,7 +71,7 @@ void CodeHelper::Timer::start()
 	result_.start = time_point_cast<microseconds>(steady_clock::now()).time_since_epoch().count();
 }
 
-void CodeHelper::Timer::stop()
+void CodeHelper::Timer::stop() noexcept
 {
 	if(finished_)
 	{
@@ -75,6 +84,13 @@ void CodeHelper::Timer::stop()
 	result_.end = time_point_cast<microseconds>(steady_clock::now()).time_since_epoch().count();
 
 	// save the result
-	result_.thread_id = std::hash<std::thread::id>{}(std::this_thread::get_id());
-	Profiler::get().write(result_);
+	try
+	{
+		result_.thread_id = std::hash<std::thread::id>{}(std::this_thread::get_id());
+		Profiler::get().write(result_);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 }
--- src/CodeHelpers/include/CodeHelpers/Profiler.hpp
+++ src/CodeHelpers/include/CodeHelpers/Profiler.hpp
@@ -2,7 +2,7 @@
 /// @brief Utility makros to benchmark and visualize code execution time
 /// @details The profiler will generate a profile_results.json file, that can be visualized with the
 /// Chrome browser by opening the file in the tracing tool.
-/// @see chrome://tracing/
+/// @see https://ui.perfetto.dev/
 /// @date 2022.11.24
 /// @author Hendrik Poettker
 
@@ -10,33 +10,62 @@
 #include <chrono>
 #include <fstream>
 #include <mutex>
+#include <perfetto/perfetto.h>
 #include <string>
 
 namespace CodeHelper
 {
 	/// @brief Contains the necessary data for the tracing tool
-	struct ProfileResult
+	struct profile_result
 	{
 		std::string name{"Default"};  ///< name of the timed block
-		long long	start{0};		  ///< begin of timing in microeconds
-		long long	end{0};			  ///< end of timing in microseconds
+		size_t		start{0};		  ///< begin of timing in microeconds
+		size_t		end{0};			  ///< end of timing in microseconds
 		size_t		thread_id{0};	  ///< needed for chrome tracing
 	};
 
-	/// @brief Writes benchmarks to the file 'profiling_results.json'
+	/// @brief Writes benchmarks to the file 'profiling_results.json' for use with
+	/// https://ui.perfetto.dev/
+	/// @details The resulting 'profiling_results.json' contains profiling data in a form, such that
+	/// browsers can display the data with the included tools. The
+	/// necessary tool for visualization is available at https://ui.perfetto.dev/. To generate
+	/// profiling data, annotate the target functions with the following makro and ensure that
+	/// PROFILING is defined
+	/// @code
+	/// { // Profiling happens in this scope
+	///		PROFILE_FUNC(); // begin timing this scope
+	///		// ...
+	///		// some code to profile
+	///		// ...
+	/// } // end timing with destructor and write to file
+	/// @endcode
 	class Profiler
 	{
 	  public:
 		/// @brief Accessor to this singleton class
+		/// @details A singleton is used here, because all profiling data is written to a single
+		/// file, which is handled by this singleton in a threadsafe way.
 		/// @return reference to the singleton
-		[[nodiscard]] static Profiler& get();
+		[[nodiscard]] static auto get() -> Profiler&;
 
 		/// @brief Add a new measurement to the file
 		/// @param result to be added
-		void write(const ProfileResult& result);
+		void write(const profile_result& result);
+
+		/// @brief Copy constructor is not used
+		Profiler(const Profiler& other) = delete;
+
+		/// @brief Move constructor is not used
+		Profiler(Profiler&& other) = delete;
+
+		/// @brief Copy assignement constructor is not used
+		auto operator=(const Profiler& other) -> Profiler& = delete;
+
+		/// @brief Move assignment is not used
+		Profiler& operator=(Profiler&& other) = delete;
 
 		/// @brief End of profiling closes the file
-		~Profiler();
+		~Profiler() noexcept;
 
 	  private:
 		/// @brief Private constructor
@@ -54,7 +83,7 @@ namespace CodeHelper
 	  public:
 		/// @brief Takes the time until this object is stopped or goes out of scope
 		/// @param name is used to differentiate different measurements
-		Timer(const std::string& name);
+		explicit Timer(const std::string& name);
 
 		/// @brief Ensures the measurement is finished
 		~Timer();
@@ -63,16 +92,16 @@ namespace CodeHelper
 		void start();
 
 		/// @brief Can be used to stop the timer before it goes out of scope
-		void stop();
+		void stop() noexcept;
 
 	  private:
-		ProfileResult result_;
-		bool		  finished_{false};
+		profile_result result_;
+		bool		   finished_{false};
 	};
 
 #ifdef PROFILING
 	#define PROFILE_SCOPE(name) CodeHelper::Timer timer##__LINE__(name)
-	#define PROFILE_FUNC() PROFILE_SCOPE(__FUNCTION__)
+	#define PROFILE_FUNC()		PROFILE_SCOPE(__FUNCTION__)
 #else
 	#define PROFILE_SCOPE(name)
 	#define PROFILE_FUNC()
