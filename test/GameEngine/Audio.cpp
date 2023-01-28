f223c52796e52fa09d5827a55b38007d2a4efe9d
Hendrik Pöttker <hendrik.poettker@web.de>
2023-01-28 13:52:12 +0100

    Added SFML::Audio (and dependency openAL32.dll)

--- test/GameEngine/Audio.cpp
+++ test/GameEngine/Audio.cpp
@@ -9,9 +9,14 @@ TEST_CASE("Audio", "[Audio]")
 	fs::path data_directory{"../../data/sounds"};
 	REQUIRE(fs::exists(data_directory));
 
-	// sf::SoundBuffer buffer; <- AUDIO CRASHES
-	// sf::Sound sound;
+	sf::SoundBuffer buffer;
+	REQUIRE(buffer.loadFromFile(data_directory / "sfx_laser1.ogg"));
 
-	// sf::SoundBuffer buffer;
-	// REQUIRE(!buffer.loadFromFile(data_directory / "sfx_laser1.ogg"));
+	sf::Sound sound;
+	sound.setBuffer(buffer);
+	sound.play();
+
+	// so the sound can play in a different thread without going out of scope
+	using namespace std::chrono_literals;
+	std::this_thread::sleep_for(0.8s); 
 }
