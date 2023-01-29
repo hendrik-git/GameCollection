e837e2f7679686ad6bd91853aa274c50b01ccd0b
Hendrik Pöttker <hendrik.poettker@web.de>
2023-01-29 16:58:16 +0100

    Added documentation to Asset class

--- src/GameEngine/include/GameEngine/Asset.hpp
+++ src/GameEngine/include/GameEngine/Asset.hpp
@@ -18,17 +18,45 @@
 class Assets
 {
   public:
+	/// @brief Default constructor
 	Assets() = default;
 
+	/// @brief Loads a font from a file and adds it to the assets data
+	/// @param name how the font will be refered to internally
+	/// @param path path to the font file
 	void add_font(std::string name, std::filesystem::path path);
+
+	/// @brief Loads a texture from a file and adds it to the assets data
+	/// @param name how the texture will be refered to internally
+	/// @param path path to the texture file
 	void add_texture(std::string name, std::filesystem::path path);
+
+	/// @brief Loads a shader from a file and adds it to the assets data
+	/// @param name how the shader will be refered to internally
+	/// @param path path to the shader file
+	/// @param type either Fragment, Vertex or Geometry shader
 	void add_shader(std::string			  name,
 					std::filesystem::path path,
 					sf::Shader::Type	  type = sf::Shader::Type::Fragment);
+
+	/// @brief Loads a sound from a file and adds it to the assets data
+	/// @param name how the sound will be refered to internally
+	/// @param path path to the sound file
 	void add_sound(const std::string& name, const std::filesystem::path& path);
 
+	/// @brief Returns a reference to a loaded font
+	/// @details e.g. for use with sf::Text
+	/// @param name of the font to load
 	[[nodiscard]] auto get_font(std::string name) -> sf::Font&;
+
+	/// @brief Returns a reference to a loaded texture
+	/// @details e.g. for use with sf::Sprite
+	/// @param name of the texture to load
 	[[nodiscard]] auto get_texture(std::string name) -> sf::Texture&;
+
+	/// @brief Returns a reference to a loaded shader
+	/// @details use with GameEngine::Component::Shader
+	/// @param name of the font to shader
 	[[nodiscard]] auto get_shader(std::string name) -> sf::Shader&;
 	[[nodiscard]] auto get_sound(std::string name) -> sf::SoundBuffer&;
 
